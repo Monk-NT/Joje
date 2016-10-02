ba71fba9bf8f13e8069424bf5c6f96a2dbfc447a
Monk-NT <monk.nt@gmail.com>
2016-10-02 23:06:53 +0200

    Implementing route

--- src/main/Web/Joje/Route.hs
+++ src/main/Web/Joje/Route.hs
@@ -2,3 +2,10 @@ module Web.Joje.Route
   (
   ) where
 
+import Network.HTTP.Types.Method
+
+data RouteData = RouteData { route :: String
+                           , verb  :: Maybe [Method]
+                           }
+
+

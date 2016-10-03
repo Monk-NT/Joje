4e7036acb7fa1d1bbc255c556ddcf8123683dc5c
Monk-NT <monk.nt@gmail.com>
2016-10-03 22:43:43 +0200

    Finding routes
    Added route finding

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -6,9 +6,10 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.HTTP.Types
 import Network.HTTP.Types.Header
+import qualified Data.ByteString.Lazy as LBS
 
 main :: IO ()
-main = joje 3000 (RouteData "/joje" Nothing demoRoute)
+main = joje 3000 [(RouteData "/joje" Nothing demoRoute), (RouteData "/snd" Nothing demoRoute)]
 
 demoRoute :: Request -> Response
-demoRoute req = responseLBS status200 [] "HelloWorld!"
+demoRoute req = responseLBS status200 [] $ LBS.fromStrict (rawPathInfo req)
--- src/main/Web/Joje.hs
+++ src/main/Web/Joje.hs
@@ -12,16 +12,23 @@ import Network.HTTP.Types.Header
 import Web.Joje.Route
 import Data.ByteString (ByteString)
 
-joje :: Port -> RouteData -> IO ()
+joje :: Port -> [RouteData] -> IO ()
 joje p routes = do
   putStrLn $ "Listening on port " ++ show p
   run p (jojeApp routes)
 
 
 
-jojeApp :: RouteData -> Application
+jojeApp :: [RouteData] -> Application
 jojeApp routes req f =
-  f $ singleRoute (rawPathInfo req) routes req
+  f $ findRoute (rawPathInfo req) routes req
+
+
+findRoute :: ByteString -> [RouteData] -> (Request -> Response)
+findRoute route [] = error ("Route " ++ show route ++ " could not be found!")
+findRoute route (rData:s)
+  | route == path rData = handler rData
+  | otherwise = findRoute route s
 
 singleRoute :: ByteString -> RouteData -> (Request -> Response)
 singleRoute route rData 

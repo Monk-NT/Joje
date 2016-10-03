a058a3e4092997aed76ba9dc4faac678884220af
Monk-NT <monk.nt@gmail.com>
2016-10-03 23:38:47 +0200

    Fixed build & lint errors
    Fixed build and lint errors reported by travis CI

--- src/main/Web/Joje.hs
+++ src/main/Web/Joje.hs
@@ -5,10 +5,8 @@ module Web.Joje
     joje
   ) where
 
-import Network.Wai (responseLBS, Application, Response, rawPathInfo, Request)
-import Network.Wai.Handler.Warp
-import Network.HTTP.Types
-import Network.HTTP.Types.Header
+import Network.Wai (Application, Response, rawPathInfo, Request)
+import Network.Wai.Handler.Warp (Port, run)
 import Web.Joje.Route
 import Data.ByteString (ByteString)
 
@@ -17,21 +15,12 @@ joje p routes = do
   putStrLn $ "Listening on port " ++ show p
   run p (jojeApp routes)
 
-
-
 jojeApp :: [RouteData] -> Application
 jojeApp routes req f =
   f $ findRoute (rawPathInfo req) routes req
 
-
-findRoute :: ByteString -> [RouteData] -> (Request -> Response)
+findRoute :: ByteString -> [RouteData] -> Request -> Response
 findRoute route [] = error ("Route " ++ show route ++ " could not be found!")
 findRoute route (rData:s)
   | route == path rData = handler rData
   | otherwise = findRoute route s
-
-singleRoute :: ByteString -> RouteData -> (Request -> Response)
-singleRoute route rData 
-  | route == path rData = handler rData
-  | otherwise = error ("No route" ++ show route)
-

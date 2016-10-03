af0e1c301916e87c1192261862120619f6e4b061
Monk-NT <monk.nt@gmail.com>
2016-10-03 23:09:28 +0200

    Fixed build errors
    Fixed build errors. Except maybe travis hlint

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -3,13 +3,12 @@
 import Web.Joje (joje)
 import Web.Joje.Route
 import Network.Wai
-import Network.Wai.Handler.Warp
 import Network.HTTP.Types
-import Network.HTTP.Types.Header
 import qualified Data.ByteString.Lazy as LBS
 
 main :: IO ()
-main = joje 3000 [(RouteData "/joje" Nothing demoRoute), (RouteData "/snd" Nothing demoRoute)]
+main = joje 3000 [ RouteData "/joje" Nothing demoRoute
+                 , RouteData "/snd" Nothing demoRoute ]
 
 demoRoute :: Request -> Response
 demoRoute req = responseLBS status200 [] $ LBS.fromStrict (rawPathInfo req)
